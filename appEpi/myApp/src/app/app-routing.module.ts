0c49d3bca61d4e6b6b7db508abd1c828c0b6fefc
scastrillg <scastrillg@eafit.edu.co>
2019-09-04 11:15:01 -0500

    botones y entradas funcionales

--- appEpi/myApp/src/app/app-routing.module.ts
+++ appEpi/myApp/src/app/app-routing.module.ts
@@ -5,7 +5,10 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+  },
+  { path: 'tab1', loadChildren: './tab1/tab1.module#Tab1PageModule' },
+  { path: 'inicio', loadChildren: './inicio/inicio.module#InicioPageModule' },
+  { path: 'encuesta', loadChildren: './encuesta/encuesta.module#EncuestaPageModule' }
 ];
 @NgModule({
   imports: [
